17e748c73c9fdc6c13cc68547d9d450cfb4acf7b
Ceayo <100067092+ceceayo@users.noreply.github.com>
2024-05-27 11:33:41 +0000

    Initial commit

---
+++ app/src/main/java/io/github/ceceayo/activitor/MainActivity.kt
@@ -0,0 +1,49 @@
+package io.github.ceceayo.activitor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.ceceayo.activitor.ui.theme.ActiviTorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ActiviTorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    Button(onClick = {
+
+                    }) {
+                        Text("Hi")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActiviTorTheme {
+        Greeting("Android")
+    }
+}
